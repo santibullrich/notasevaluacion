1e50127151b49f66af6055eb8daa2c4a4e7d7cbc
santibullrich <santibullrich@gmail.com>
2021-01-15 12:21:56 -0300

    ultima actualizacion

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,7 +4,13 @@ const router = express.Router();
 const indexController = require('../controllers/indexController');
 const notasController = require('../controllers/notasController');
 
+// ir a la home
 router.get('/', indexController.index)
+
+// crear nota
 router.post('/', notasController.create)
+
+//borrar nota
+router.delete('/', notasController.destroy);
 
 module.exports = router;
